7a0f3e348b6058e725ccc33b5bea26f60fa53832
codyn06 <nguyencody2006@gmail.com>
2025-11-15 02:19:22 -0500

    Added comments to explain code for readibility

--- flyer.py
+++ flyer.py
@@ -1,11 +1,13 @@
 import os
 
 def generate_flyer(topic, description, date, time, location, bg = None):
+    
     script_dir = os.path.dirname(os.path.abspath(__file__))
     template_path = os.path.join("assets", "template.svg")
     with open(template_path, "r", encoding="utf-8") as file:
         svg_data = file.read()
 
+    # Replaces data in the template
     svg_data = svg_data.replace("TOPIC", topic)
     svg_data = svg_data.replace("DESCRIPTION", description)
     svg_data = svg_data.replace("DATE", date)
@@ -17,10 +19,13 @@ def generate_flyer(topic, description, date, time, location, bg = None):
 
     # image_tag = f'<image x="0" y="0" width="612" height="792" href="{bg}" />\n'
     # svg_data = svg_data.replace("</defs>", f"</defs>\n{image_tag}")
-
+    
+    # Creates an output folder
     output_dir = os.path.join(script_dir, "output")
     os.makedirs(output_dir, exist_ok=True)
 
+
+    # Places creation in the output folder
     output_path = os.path.join(output_dir, "flyer_output.svg")
     with open(output_path, "w", encoding="utf-8") as file:
         file.write(svg_data)
