905088cf975870d53afb1af7a45f81fa5041451c
codyn06 <nguyencody2006@gmail.com>
2025-11-15 02:17:23 -0500

    Added creation of the output file + updated .gitignore

--- flyer.py
+++ flyer.py
@@ -1,6 +1,7 @@
 import os
 
 def generate_flyer(topic, description, date, time, location, bg = None):
+    script_dir = os.path.dirname(os.path.abspath(__file__))
     template_path = os.path.join("assets", "template.svg")
     with open(template_path, "r", encoding="utf-8") as file:
         svg_data = file.read()
@@ -17,7 +18,11 @@ def generate_flyer(topic, description, date, time, location, bg = None):
     # image_tag = f'<image x="0" y="0" width="612" height="792" href="{bg}" />\n'
     # svg_data = svg_data.replace("</defs>", f"</defs>\n{image_tag}")
 
-    output_path = os.path.join("output", "flyer_output.svg")
+    output_dir = os.path.join(script_dir, "output")
+    os.makedirs(output_dir, exist_ok=True)
+
+    output_path = os.path.join(output_dir, "flyer_output.svg")
     with open(output_path, "w", encoding="utf-8") as file:
         file.write(svg_data)
+
     return output_path
